2a2b3e15232ec1e10325f2a5859f272d304060ea
KadirTopal <kadirkenantopal@gmail.com>
2022-09-10 21:57:47 -0400

    updated

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const addForm = document.getElementById('add-form');
-
 addForm.addEventListener('submit', addTask);
+
+//variables
+let total = 0;
+let completed = 0;
+
+
+document.addEventListener('click', (e)=>{
+  if(e.target.className.includes('delete-icon')){
+    const span = e.target
+    const btn = span.parentElement;
+    const task = btn.parentElement;
+
+    const checkBox = btn.previousElementSibling.firstElementChild;
+    if(checkBox.checked){
+      completed -= 1;
+      document.getElementById('completed').textContent = completed;
+    }
+    task.remove();
+    total -= 1;
+    document.getElementById('total').textContent = total;
+  }
+})
+
+document.addEventListener('change', (e)=>{
+  if(e.target.className.includes('finish-box') && e.target.checked){
+    const task = e.target.parentElement.parentElement;
+    task.style.backgroundColor = 'lightgreen';
+    // increase the completed tasks number by 1
+    completed += 1;
+    document.getElementById('completed').textContent = completed;
+  } else if (e.target.className.includes('finish-box') && !e.target.checked){
+    //decrease the uncomplated tasks
+    completed -= 1;
+    document.getElementById('completed').textContent = completed;
+    const task = e.target.parentElement.parentElement;
+    task.style.backgroundColor = '#fefefe'
+  }
+})
+
 
 
 // Adding a task
@@ -16,8 +54,13 @@ function addTask(e){
   card.classList.add('card');
   card.innerHTML = `
     <div>
-      <input type="checkbox">
+      <input type="checkbox" class="finish-box">
       <span> ${enteredValue}</span>
+      <p class="date">${new Date().toLocaleDateString('en-us', {
+        year: 'numeric',
+        month: 'long',
+        day:'numeric'
+      })} </p>
     </div>
     <button class="delete-btn"> 
       <span class="material-symbols-outlined delete-icon">
@@ -32,10 +75,9 @@ function addTask(e){
   // Clear the input
   inputField.value = '';
 
-
+  // update the app stats with total number of tasks
+  total += 1;
+  document.getElementById('total').textContent = total;
 
 
 }
-
-
-// 1.52
